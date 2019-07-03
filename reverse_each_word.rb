9463ddc1b0ee237676d62316688f0f3ab19789fd
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-07-03 21:57:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   new_str = ""
   words = string.split(" ")
-  words.collect {|word| word.reverse!}
+  words.collect do |word|
+    word.reverse 
   end
   words.join(" ")
   words
